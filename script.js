187f6c2badf8566caf98ee4151e1dfdd7fb816ea
pui-yi <153875169+pui-yi@users.noreply.github.com>
2023-12-19 15:11:09 -0800

    Update script.js
    Fix bug: Now it presents a descriptive text, the term, in the x axis of the graphic. The tooltip now presents the content on the x axis, the term, in respective to the content on the y axis, the number of students.

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ let num_students = [];
       d3.json("hw3data.json")
         .then((data) => {
           for (key in data) {
-          num_students.push(+data[key].students)
+            num_students.push(+data[key].students)
           } 
           for (key in data) {
-          term.push(data[key])
+            term.push(data[key].term)
           } 
-          console.log(term);
         let tooltip = d3.select('body')
           .append('div')
           .style('position', 'absolute')
@@ -16,7 +15,7 @@ let num_students = [];
           .style('background', 'black')
           .style('opacity', 0);
 
-        let margin = { top: 20, right: 20, bottom: 30, left: 40 };
+        let margin = { top: 20, right: 20, bottom: 40, left: 50 };
       
         let height = 200 - margin.top - margin.bottom, width = 500 - margin.left - margin.right, barW = 40, barSpace = 5;
         
@@ -30,7 +29,7 @@ let num_students = [];
           .range([0, height]);
 
         let xS = d3.scaleBand()
-          .domain(d3.range(0, num_students.length))
+          .domain(data.map(d => d.term))
           .range([0, width])
           .padding(0.1);
 
@@ -43,35 +42,49 @@ let num_students = [];
           .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
         
         graph.selectAll('rect')
-          .data(num_students)
+          .data(data)
           .enter()
           .append('rect')
           .style('fill', 'blue')
           .attr('width', xS.bandwidth)
-          .attr('height', function(d) { return yS(d); })
-          .attr('x', function(d, i) { return xS(i); })
-          .attr('y', function(d) { return height - yS(d); })
+          .attr('height', function(d) { return yS(d.students); })
+          .attr('x', function(d, i) { return xS(d.term); })
+          .attr('y', function(d) { return height - yS(d.students); })
           .on('mouseover', function(event, d) {
               const[x, y] = d3.pointer(event)
               tooltip.transition()
-              .style('opacity', .8)
-              tooltip.html(d)
-              .style('left', (x + 75) + 'px')
-              .style('top', (y + 30) + 'px')
+                .style('opacity', 0.8)
+              tooltip.html(`Term: ${d.term}<br>Students: ${d.students}`)
+                .style('left', (x + 75) + 'px')
+                .style('top', (y + 30) + 'px')
               d3.select(this)
-              .style('opacity', .4)
+                .style('opacity', 0.4)
           })
           .on('mouseout', function(event, d) {
             d3.select(this)
               .style('opacity', 1)
           });
 
-          graph.append('g')
-          .call(d3.axisLeft(yS).scale(verticalGuide));
-          graph.append('g')
+        graph.append('g')
+            .call(d3.axisLeft(yS).scale(verticalGuide));
+          
+        graph.append('g')
           .call(d3.axisBottom(xS))
           .attr('transform', 'translate(0, ' + height + ')');
-          
+        
+        graph.append("g")
+          .append("text")
+          .attr("transform", "translate (-35, 30) rotate(-90)")
+          .attr("stroke", "black")
+          .classed("axis text", true)
+          .text("Number of students");
+
+        graph.append("g")
+          .append("text")
+          .attr("transform", "translate(220," + 170 + ")")
+          .attr("stroke", "black")
+          .classed("axis text", true)
+          .text("Term");
       })
         .catch(() => {
             console.log("Something was not right! No data!");
